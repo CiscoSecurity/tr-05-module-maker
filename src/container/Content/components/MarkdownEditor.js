8bd2b516cdb04f0f5e44853abe4544fcb460ea60
anastasiia.rozlyvan <nastia.2.r.2@gmail.com>
2020-10-16 13:27:46 +0300

    Change Showdown to ReactMarkdown

--- src/container/Content/components/MarkdownEditor.js
+++ src/container/Content/components/MarkdownEditor.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactMde from "react-mde";
-import * as Showdown from "showdown";
+import ReactMarkdown from "react-markdown";
 import "react-mde/lib/styles/css/react-mde-all.css";
 import { updateSingleInput } from "container/Content/otherInputsActions";
 import { connect } from "react-redux";
 
-
-const converter = new Showdown.Converter({
-    tables: true,
-    simplifiedAutoLink: true,
-    strikethrough: true,
-    tasklists: true
-});
 
 class MarkdownEditor extends React.Component {
     constructor(props) {
@@ -45,8 +38,8 @@ class MarkdownEditor extends React.Component {
             onChange={this.handleValueChange}
             onTabChange={this.handleTabChange}
             value={this.state.value}
-            generateMarkdownPreview={markdown =>
-                Promise.resolve(converter.makeHtml(markdown))
+            generateMarkdownPreview={(markdown) =>
+                Promise.resolve(<ReactMarkdown source={markdown} />)
             }
             selectedTab={this.state.tab}
         />
