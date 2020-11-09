e67068cdc6daa9018caa019aa7a29f3990b03482
anastasiia.rozlyvan <nastia.2.r.2@gmail.com>
2020-11-09 10:03:20 +0200

    Updates

---
+++ precache-manifest.f464c777af64c40f820e50ca96527213.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5884f5d9b944e6a070390340e185b43",
+    "url": "/tr-05-module-maker/index.html"
+  },
+  {
+    "revision": "f754a30e79f5c20ea725",
+    "url": "/tr-05-module-maker/static/css/2.0f8c67e6.chunk.css"
+  },
+  {
+    "revision": "b01982b33679483f64f6",
+    "url": "/tr-05-module-maker/static/css/main.6456e237.chunk.css"
+  },
+  {
+    "revision": "f754a30e79f5c20ea725",
+    "url": "/tr-05-module-maker/static/js/2.a73872ca.chunk.js"
+  },
+  {
+    "revision": "6bd559ff3df70d16b0e7a601898417cc",
+    "url": "/tr-05-module-maker/static/js/2.a73872ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01982b33679483f64f6",
+    "url": "/tr-05-module-maker/static/js/main.7c568aa8.chunk.js"
+  },
+  {
+    "revision": "a78000d55bd8e89dd3c7",
+    "url": "/tr-05-module-maker/static/js/runtime-main.d588aec7.js"
+  },
+  {
+    "revision": "ff84a0dda4f5b41dca374d915a1e9c84",
+    "url": "/tr-05-module-maker/static/media/CiscoSansTTLight.ff84a0dd.woff"
+  },
+  {
+    "revision": "aeaec016d6766e2a0f4dfeaec2c0a629",
+    "url": "/tr-05-module-maker/static/media/CiscoSansTTRegular.aeaec016.woff"
+  },
+  {
+    "revision": "31710dea9e654299c09e7e22e01f04be",
+    "url": "/tr-05-module-maker/static/media/sprite.31710dea.svg"
+  }
+]);
